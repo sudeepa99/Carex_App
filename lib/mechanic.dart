b318229abb4e9213a512d0e3654150518572195c
Vishmikaushalya <garuvish999@gmail.com>
2024-02-04 20:39:14 +0530

    Test Branch finished

--- lib/mechanic.dart
+++ lib/mechanic.dart
@@ -102,7 +102,7 @@ class _MyWidgetState extends State<MechanicPage> {
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
                             Text(
-                              "Toyota x",
+                              "TOYOTA",
                               style: TextStyle(
                                 fontSize: 20.0,
                                 color: Color(0XFFFFFFFF),
