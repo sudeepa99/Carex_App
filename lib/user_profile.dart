f48d449720e5797184c2a71dfc4d67b2db9ed75d
sudeepa99 <sudeepa1234nisal@gmail.com>
2024-02-15 23:20:52 +0530

    Added user profile

---
+++ lib/user_profile.dart
@@ -0,0 +1,298 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class UserProfile extends StatelessWidget {
+  const UserProfile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 0.0),
+            color: const Color(0XFF22252B),
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 30),
+                    child: IconButton(
+                      icon: const Icon(Icons.arrow_back),
+                      color: Colors.white,
+                      iconSize: 35,
+                      onPressed: () {},
+                    ),
+                  ),
+                  const Center(
+                    child: Text(
+                      "Profile",
+                      style: TextStyle(fontSize: 22, color: Colors.white),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 7,
+                  ),
+                  const Divider(
+                    color: Color(0xFFFF7817),
+                  ),
+                  Row(
+                    children: [
+                      const Text(
+                        "Profile Photo",
+                        style: TextStyle(fontSize: 22, color: Colors.white),
+                      ),
+                      const SizedBox(
+                        width: 144,
+                      ),
+                      InkWell(
+                        child: Image.asset("assets/Ellipse 36.png"),
+                        onTap: () {},
+                      ),
+                      const SizedBox(
+                        width: 5,
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.arrow_forward_ios,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const Divider(
+                    color: Color(0xFFFF7817),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                      const Color(0XFF22252B),
+                    )),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          "First Name",
+                          style: TextStyle(
+                              color: Colors.white.withOpacity(0.6),
+                              fontSize: 20),
+                        ),
+                        const SizedBox(
+                          width: 200,
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.arrow_forward_ios,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Divider(
+                    color: Colors.white.withOpacity(0.4),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                      const Color(0XFF22252B),
+                    )),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Last Name",
+                          style: TextStyle(
+                              color: Colors.white.withOpacity(0.6),
+                              fontSize: 20),
+                        ),
+                        const SizedBox(
+                          width: 200,
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.arrow_forward_ios,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Divider(
+                    color: Colors.white.withOpacity(0.4),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                      const Color(0XFF22252B),
+                    )),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          "NIC Number",
+                          style: TextStyle(
+                              color: Colors.white.withOpacity(0.6),
+                              fontSize: 20),
+                        ),
+                        const SizedBox(
+                          width: 185,
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.arrow_forward_ios,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Divider(
+                    color: Colors.white.withOpacity(0.4),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                      const Color(0XFF22252B),
+                    )),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Contact Number",
+                          style: TextStyle(
+                              color: Colors.white.withOpacity(0.6),
+                              fontSize: 20),
+                        ),
+                        const SizedBox(
+                          width: 146,
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.arrow_forward_ios,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Divider(
+                    color: Colors.white.withOpacity(0.4),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                      const Color(0XFF22252B),
+                    )),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Email Address",
+                          style: TextStyle(
+                              color: Colors.white.withOpacity(0.6),
+                              fontSize: 20),
+                        ),
+                        const SizedBox(
+                          width: 163,
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.arrow_forward_ios,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Divider(
+                    color: Colors.white.withOpacity(0.4),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                      const Color(0XFF22252B),
+                    )),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Adress",
+                          style: TextStyle(
+                              color: Colors.white.withOpacity(0.6),
+                              fontSize: 20),
+                        ),
+                        const SizedBox(
+                          width: 230,
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.arrow_forward_ios,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Divider(
+                    color: Colors.white.withOpacity(0.4),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                      const Color(0XFF22252B),
+                    )),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Password",
+                          style: TextStyle(
+                              color: Colors.white.withOpacity(0.6),
+                              fontSize: 20),
+                        ),
+                        const SizedBox(
+                          width: 205,
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.arrow_forward_ios,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Divider(
+                    color: Colors.white.withOpacity(0.4),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
