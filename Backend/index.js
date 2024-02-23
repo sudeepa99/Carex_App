e32099b97e63c12c04969b6151689edf797ca825
Amagunarathna <amandagunarathna3021@gmail.com>
2024-02-23 11:45:59 +0530

    Add mongodb connection

--- Backend/index.js
+++ Backend/index.js
@@ -8,6 +8,20 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 8000;
 
+// database connection
+mongoose.set("strictQuery", false);
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB database connected");
+  } catch (err) {
+    console.log("MongoDB database not connected");
+  }
+};
+
 //for testing
 app.get("/",(req,res)=>{
  res.send("api is working. This this vehicle breakdown help app. I work with backend of this project.");
@@ -16,10 +30,11 @@ app.get("/",(req,res)=>{
 
 app.use(express.json());
 //app.use(cors(corsOptions));
+
 app.use(cookieParser());
 
 
 app.listen(port, () => {
-    //connect();
+    connect();
     console.log("Server listing on port", port);
   });
