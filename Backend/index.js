6657b7a760491ff5d6b7f0f2e2883b2ecb472532
Amagunarathna <amandagunarathna3021@gmail.com>
2024-02-24 02:07:06 +0530

    Update rotes

--- Backend/index.js
+++ Backend/index.js
@@ -1,20 +1,28 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-//import cors from "cors";
-import cookieParser from "cookie-parser";
+import cors from "cors";
+//import cookieParser from "cookie-parser";
+import userRoute from "./routes/user.js";
+import authRoute from "./routes/auth.js";
+
+
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 8000;
+const corsOptions = {
+  origin: true,
+  credentials: true,
+};
 
 // database connection
 mongoose.set("strictQuery", false);
 const connect = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      //useNewUrlParser: true,
+      //useUnifiedTopology: true,
     });
     console.log("MongoDB database connected");
   } catch (err) {
@@ -24,14 +32,23 @@ const connect = async () => {
 
 //for testing
 app.get("/",(req,res)=>{
- res.send("api is working. This this vehicle breakdown help app. I work with backend of this project.");
+ res.send("api is working. This this vehicle breakdown help app. I work with backend of this project. Nowadays codinggg....");
 });
 
 
 app.use(express.json());
-//app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 
-app.use(cookieParser());
+//app.use(cookieParser());
+
+//app.use("/api/auth", authRoute);
+//app.use("/api/users", userRoute);
+
+app.use("/api/v1/auth", authRoute);
+
+app.use("/api/v1/users", userRoute);
+
+
 
 
 app.listen(port, () => {
