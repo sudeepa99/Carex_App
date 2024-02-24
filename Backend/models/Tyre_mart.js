c8bfabdae4837957838cdb6b111317e9e1cd38d8
Amagunarathna <amandagunarathna3021@gmail.com>
2024-02-25 00:13:38 +0530

    Create tyre_mart model

---
+++ Backend/models/Tyre_mart.js
@@ -0,0 +1,61 @@
+import mongoose from "mongoose";
+
+const tyre_martSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    userEmail: {
+        type: String,
+      },
+      
+      fullName: {
+        type: String,
+        required: true,
+      },
+    city: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    distance: {
+      type: Number,
+      required: true,
+    },
+    photo: {
+      type: String,
+      required: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+
+    appoinment: {
+      type: String,
+      required: true,
+    },
+  
+    vehicle: {
+      type: String,
+      required: true,
+    },
+    
+    
+
+    
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Tyre_mart", tyre_martSchema);
