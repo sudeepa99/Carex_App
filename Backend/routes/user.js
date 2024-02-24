1d31607f17311b7813abfc703af1e807c45567ec
Amagunarathna <amandagunarathna3021@gmail.com>
2024-02-25 00:12:18 +0530

    Modified

--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -1,13 +1,21 @@
 import express from 'express'
-import {  updateUser,deleteUser} from '../controllers/userController.js'
+import { updateUser,deleteUser,getSingleUser,getAllUser } from '../controllers/userController.js';
+
 
 const router = express.Router();
-import { verifyUser } from '../utils/verifyToken.js';
+import { verifyAdmin,verifyUser } from '../utils/verifyToken.js';
+
 //update User
 router.put('/:id',verifyUser,updateUser);
 //delete User 
 
 router.delete('/:id',verifyUser,deleteUser);
+
+//get Single User
+router.get('/:id',verifyUser,getSingleUser);
+
+//get All User
+router.get('/',verifyAdmin,getAllUser);
 
 
 export default router;
