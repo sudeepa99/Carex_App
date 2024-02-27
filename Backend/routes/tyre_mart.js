f688adc24109e3ba886dfccfcbf38a75f8535fa4
Amagunarathna <amandagunarathna3021@gmail.com>
2024-02-28 03:22:15 +0530

    Get tyre_mart by search

---
+++ Backend/routes/tyre_mart.js
@@ -0,0 +1,38 @@
+import express from "express";
+import {
+  //createTyreMart,
+  //updateTyreMart,
+  //deleteTyreMart,
+  //getSingleTyreMart,
+  //getAllTyreMart,
+  getTyre_martBysearch,
+  //getFeaturedTyreMart,
+  //getTyreMartCount,
+} from "./../controllers/tyre_martController.js";
+//import { verifyAdmin } from "../utils/verifyToken.js";
+
+const router = express.Router();
+
+//create new tyre mart  
+//router.post("/", verifyAdmin, createTyreMart);
+//update tyre mart  
+//router.put("/:id", verifyAdmin, updateTyreMart);
+// //delete tyre mart    
+
+//router.delete("/:id", verifyAdmin, deleteTyreMart);
+
+// //getSingle tyre mart   
+//router.get("/:id", getSingleTyreMart);
+
+// //getAll tyre mart  
+//router.get("/", getAllTyreMart);
+
+//get tyre mart   by search
+router.get("/search/getTyre_martBySearch", getTyre_martBysearch);
+
+// //get tyre mart   by search
+//router.get("/search/getFeaturedTyreMart", getFeaturedTyreMart);
+// //get tyre mart   by search
+//router.get("/search/getTyreMartCount", getTyreMartCount);
+
+export default router;
