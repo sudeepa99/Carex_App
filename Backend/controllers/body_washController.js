388cc6463488271c1a095a79ef48bcbbf6d4b78b
Amagunarathna <amandagunarathna3021@gmail.com>
2024-03-08 02:35:31 +0530

    Add search online body wash

--- Backend/controllers/body_washController.js
+++ Backend/controllers/body_washController.js
@@ -1,5 +1,49 @@
 import Body_wash from "../models/Body_wash.js";
 
+//getSingle body wash
+export const getSingleBodyWash = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const body_wash = await Body_wash.findById(id);
+    res.status(200).json({
+      success: true,
+      message: "Successfully",
+      data: body_wash,
+    });
+  } catch (err) {
+    res.status(404).json({ success: false, message: "Not found" });
+  }
+};
+
+
+
+
+ //getAllbodywashes
+ export const getAllBodyWash = async (req, res) => {
+  //for pagination
+  const page = parseInt(req.query.page);
+  console.log(page);
+  try {
+    const body_washes = await Body_wash.find({})
+      //.populate("reviews")
+      .skip(page * 8)
+      .limit(8);
+    res.status(200).json({
+      success: true,
+      count: body_washes.length,
+      message: "Successfully",
+      data: body_washes,
+    });
+  } catch (err) {
+    res.status(404).json({ success: false, message: "Not found" });
+  }
+};
+
+
+
+
+
+
 //get body_wash by search
 export const getBody_washBysearch = async (req, res) => {
   //here i means case sensitive
@@ -10,7 +54,7 @@ export const getBody_washBysearch = async (req, res) => {
   try {
     //get means greater than equal
     const body_wash = await Body_wash.find({
-      userName,
+      fullName,
       from,
       //maxGroupSize: { $gte: maxGroupSize },
     });
@@ -24,3 +68,21 @@ export const getBody_washBysearch = async (req, res) => {
     res.status(404).json({ success: false, message: "Not found" });
   }
 };
+
+ //get online body wash
+ export const getOnlineBodyWash = async (req, res) => {
+  try {
+    const body_washes = await Body_wash.find({ online: true })
+      
+      .limit(8);
+    res.status(200).json({
+      success: true,
+      count: body_washes.length,
+      message: "Successfully",
+      data: body_washes,
+    });
+  } catch (err) {
+    res.status(404).json({ success: false, message: "Not found" });
+  }
+};
+
